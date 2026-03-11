4d7267571cd5029c50462ffcbc2117e0198dc4fd
Olam0436 <claudio.chimenti@uniroma1.it>
2026-03-11 14:21:56 +0100

    17

--- generateTokens.js
+++ generateTokens.js
@@ -1,13 +1,13 @@
 const sqlite3 = require("sqlite3").verbose();
 const QRCode = require("qrcode");
 const { v4: uuidv4 } = require("uuid");
+const path = require("path");
 
 const db = new sqlite3.Database("votes.db");
 
 const PARTICIPANTI = 300; // numero partecipanti
 
-db.serialize(()=>{
-
+db.serialize(() => {
   db.run(`
     CREATE TABLE IF NOT EXISTS tokens(
       token TEXT PRIMARY KEY,
@@ -15,18 +15,18 @@ db.serialize(()=>{
     )
   `);
 
-  for(let i=0;i<PARTICIPANTI;i++){
-    let token = uuidv4();
+  for (let i = 0; i < PARTICIPANTI; i++) {
+    const token = uuidv4();
+    db.run("INSERT INTO tokens(token) VALUES(?)", [token]);
 
-    db.run("INSERT INTO tokens(token) VALUES(?)",[token]);
+    // Sostituisci "votazione-1" con il tuo dominio Render
+    const url = `https://votazione-1.onrender.com/vote.html?token=${token}`;
 
-    let url = `https://votazione-1.onrender.com/vote.html?token=${token}`;
-
-    QRCode.toFile(`qr-${i+1}.png`, url, (err)=>{
-      if(err) console.log("Errore QR:", err);
+    // Salva i PNG nella cartella "qrcodes"
+    QRCode.toFile(path.join(__dirname, "qrcodes", `qr-${i + 1}.png`), url, (err) => {
+      if (err) console.log("Errore QR:", err);
     });
   }
 
   console.log(`${PARTICIPANTI} token generati!`);
-
 });
